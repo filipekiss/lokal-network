9bc0cde5f807610b94c7b2b3e37de0f05cabbb82
Filipe Kiss <hello@filipekiss.com.br>
2019-07-10 17:41:56 +0200

    First Release

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Quicksand', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
